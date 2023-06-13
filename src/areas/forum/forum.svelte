7741d6c0afe5bcc35c2f9d0fb7e1409b50e2fd1b
Gang-Sang <sangx001@gmail.com>
2023-06-12 22:49:13 -0500

    Add SSR data loading

---
+++ src/areas/forum/forum.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import type { ForumModel } from '../../common/types/pageModels';
+
+	export let model: ForumModel;
+</script>
+
+<div>
+	<div>
+		<div>{model.forum?.name}</div>
+		<div>{model.forum?.description}</div>
+	</div>
+	<div>
+		<div>POST</div>
+		{#each model.posts as post (post.id)}
+			<div>
+				<div>{post.title}</div>
+				<div>{post.content}</div>
+			</div>
+		{/each}
+	</div>
+</div>
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,27 +1,11 @@
-import type { Forum, Post } from '../common/types/dataTypes';
+import * as config from '../../static/config.json';
+import type { Forum } from '../common/types/dataTypes';
 import type { HomeModel } from '../common/types/pageModels';
 
 export async function load() {
+	const url = `${config.api}/forum`;
+	const forums: Forum[] = await fetch(url).then((res) => res.json());
 	return {
-		forumCategories: [
-			{
-				id: 1,
-				name: 'Public Forums',
-				description: 'A general discussion forum',
-				forums: [
-					{ id: 1, name: 'General Discussion', description: 'A general discussion forum', lastPost: { id: 1, title: 'Welcome to the new forum!', content: 'Welcome to the new forum!', created: new Date('2023-03-02T12:00:00') } as Post },
-					{ id: 2, name: 'News', description: 'News and announcements', lastPost: { id: 2, title: 'Welcome to the new forum!', content: 'Welcome to the new forum!', created: new Date('2023-03-02T12:00:00') } as Post },
-				] as Forum[],
-			},
-			{
-				id: 2,
-				name: 'Member Forums',
-				description: 'Private forums for members only',
-				forums: [
-					{ id: 3, name: 'Member Discussion', description: 'A general discussion forum', lastPost: { id: 3, title: 'Welcome to the new forum!', content: 'Welcome to the new forum!', created: new Date('2023-03-02T12:00:00') } as Post },
-					{ id: 4, name: 'Member News', description: 'News and announcements', lastPost: { id: 4, title: 'Welcome to the new forum!', content: 'Welcome to the new forum!', created: new Date('2023-03-02T12:00:00') } as Post },
-				] as Forum[],
-			},
-		]
+		forums
 	} as HomeModel;
 }
